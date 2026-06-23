73f99c0656c4e0bd0a720cb673a5750a1dadc185
iZcy <121650790+iZcy@users.noreply.github.com>
2026-06-23 19:19:07 +0700

    Replace PricingTable with hardcoded ServicesOption on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,11 @@
 import { Helmet } from "react-helmet-async";
 import { useCollection } from "@/context/DataContext";
 import Hero from "@/components/MainPage/Hero/Hero";
-import PricingTable from "@/components/MainPage/PricingTable";
+import ServicesOption from "@/components/Services/ServicesOption/ServicesOption";
 import PortoSection from "@/components/MainPage/PortoSection";
 import CTA from "@/components/MainPage/CTA";
 
 export default function Home() {
-  const [pricing] = useCollection("pricing");
   const [projects] = useCollection("projects");
 
   return (
@@ -15,7 +14,7 @@ export default function Home() {
         <title>ArachnoVa</title>
       </Helmet>
       <Hero />
-      <PricingTable data={pricing} />
+      <ServicesOption />
       <PortoSection projects={projects} />
       <CTA />
     </>
