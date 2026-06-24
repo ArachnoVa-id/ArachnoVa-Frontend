19be6670a90e3882e26029b3429704ac08fcf4d7
iZcy <121650790+iZcy@users.noreply.github.com>
2026-06-25 05:40:34 +0700

    Revert panel to light, use dark VS Code Light theme colors for code text

--- src/components/ui/CodeTyper.jsx
+++ src/components/ui/CodeTyper.jsx
@@ -1,67 +1,18 @@
 import { useState, useEffect } from "react";
 
 const codeData = [
-  [
-    { c: "#569CD6", t: "import " },
-    { c: "#CE9178", t: '"./globals.css";' },
-  ],
-  [
-    { c: "#569CD6", t: "import " },
-    { c: "#D4D4D4", t: "{ " },
-    { c: "#4FC1FF", t: "ArachnoVa" },
-    { c: "#D4D4D4", t: " } from " },
-    { c: "#CE9178", t: '"./api/provider";' },
-  ],
-  [
-    { c: "#D4D4D4", t: "\u00A0" },
-  ],
-  [
-    { c: "#569CD6", t: "export default" },
-  ],
-  [
-    { c: "#D4D4D4", t: "<" },
-    { c: "#DCDCAA", t: "div" },
-    { c: "#D4D4D4", t: " " },
-    { c: "#9CDCFE", t: "className" },
-    { c: "#D4D4D4", t: '="' },
-    { c: "#CE9178", t: "Welcome to ArachnoVa" },
-    { c: "#D4D4D4", t: '">' },
-  ],
-  [
-    { c: "#D4D4D4", t: "\u00A0 <" },
-    { c: "#DCDCAA", t: "div" },
-    { c: "#D4D4D4", t: " " },
-    { c: "#9CDCFE", t: "className" },
-    { c: "#D4D4D4", t: '="' },
-    { c: "#CE9178", t: "content" },
-    { c: "#D4D4D4", t: '">' },
-  ],
-  [
-    { c: "#D4D4D4", t: "\u00A0 {" },
-  ],
-  [
-    { c: "#D4D4D4", t: "\u00A0\u00A0 " },
-    { c: "#CE9178", t: '"Your web design partner"' },
-  ],
-  [
-    { c: "#D4D4D4", t: "\u00A0 }" },
-  ],
-  [
-    { c: "#D4D4D4", t: "\u00A0 " },
-    { c: "#DCDCAA", t: "</div>" },
-  ],
-  [
-    { c: "#D4D4D4", t: "\u00A0 <" },
-    { c: "#DCDCAA", t: "img" },
-    { c: "#D4D4D4", t: " " },
-    { c: "#9CDCFE", t: "src" },
-    { c: "#D4D4D4", t: '="' },
-    { c: "#CE9178", t: "img/herofix.png" },
-    { c: "#D4D4D4", t: '"/>' },
-  ],
-  [
-    { c: "#DCDCAA", t: "</div>" },
-  ],
+  [{ c: "#0000CC", t: "import " }, { c: "#A31515", t: '"./globals.css";' }],
+  [{ c: "#0000CC", t: "import " }, { c: "#333333", t: "{ " }, { c: "#001080", t: "ArachnoVa" }, { c: "#333333", t: " } from " }, { c: "#A31515", t: '"./api/provider";' }],
+  [{ c: "#333333", t: "\u00A0" }],
+  [{ c: "#0000CC", t: "export default" }],
+  [{ c: "#333333", t: "<" }, { c: "#795E26", t: "div" }, { c: "#333333", t: " " }, { c: "#001080", t: "className" }, { c: "#333333", t: '="' }, { c: "#A31515", t: "Welcome to ArachnoVa" }, { c: "#333333", t: '">' }],
+  [{ c: "#333333", t: "\u00A0 <" }, { c: "#795E26", t: "div" }, { c: "#333333", t: " " }, { c: "#001080", t: "className" }, { c: "#333333", t: '="' }, { c: "#A31515", t: "content" }, { c: "#333333", t: '">' }],
+  [{ c: "#333333", t: "\u00A0 {" }],
+  [{ c: "#333333", t: "\u00A0\u00A0 " }, { c: "#A31515", t: '"Your web design partner"' }],
+  [{ c: "#333333", t: "\u00A0 }" }],
+  [{ c: "#333333", t: "\u00A0 " }, { c: "#795E26", t: "</div>" }],
+  [{ c: "#333333", t: "\u00A0 <" }, { c: "#795E26", t: "img" }, { c: "#333333", t: " " }, { c: "#001080", t: "src" }, { c: "#333333", t: '="' }, { c: "#A31515", t: "img/herofix.png" }, { c: "#333333", t: '"/>' }],
+  [{ c: "#795E26", t: "</div>" }],
 ];
 
 export default function CodeTyper({ speed = 70, className, onDone }) {
@@ -69,22 +20,14 @@ export default function CodeTyper({ speed = 70, className, onDone }) {
   const [wordIdx, setWordIdx] = useState(0);
 
   useEffect(() => {
-    if (lineIdx >= codeData.length) {
-      return;
-    }
+    if (lineIdx >= codeData.length) return;
     const maxW = codeData[lineIdx].length;
     if (wordIdx < maxW) {
       const t = setTimeout(() => setWordIdx((v) => v + 1), speed);
       return () => clearTimeout(t);
     }
-    if (lineIdx === codeData.length - 1) {
-      onDone?.();
-      return;
-    }
-    const t = setTimeout(() => {
-      setLineIdx((v) => v + 1);
-      setWordIdx(0);
-    }, speed * 2.5);
+    if (lineIdx === codeData.length - 1) { onDone?.(); return; }
+    const t = setTimeout(() => { setLineIdx((v) => v + 1); setWordIdx(0); }, speed * 2.5);
     return () => clearTimeout(t);
   }, [lineIdx, wordIdx, speed, onDone]);
 
